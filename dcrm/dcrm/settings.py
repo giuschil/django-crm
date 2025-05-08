0c419fb0721ea4ffa5fee7a344f75050e1981032
Giuseppe Schillaci <gschillaci@epayworldwide.com>
2025-05-08 12:10:02 +0200

    modify static files settings

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -112,6 +112,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "theme/static_src/src"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
